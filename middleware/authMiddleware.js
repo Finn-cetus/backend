efa8ab28f45d6d09f9adebf853461616e810dd17
Finn-cetus <chunggtest2006@gmail.com>
2025-09-09 16:45:49 +0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,13 @@
-const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-module.exports = function(req, res, next) {
-    const token = req.header('x-auth-token');
-
-    if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
-    }
-
+module.exports = async function(req, res, next) {
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
+        const user = await User.findById(req.user.id);
+        if (user.role !== 'admin') {
+            return res.status(403).json({ msg: 'Access denied. Admin resource.' });
+        }
         next();
     } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
+        res.status(500).send('Server Error');
     }
 };
